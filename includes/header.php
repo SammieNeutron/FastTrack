f10b7fc5db0bf7636307294b0fa8f99db044ba63
sammieboy <samsonadegboyega@gmail.com>
2019-11-28 17:03:31 +0100

    first website commit

---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<header class="site-navbar py-3" role="banner">
+  <div class="container">
+    <div class="row align-items-center">
+      
+      <div class="col-11 col-xl-2">
+        <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0">FastTrack</a></h1>
+      </div>
+      <div class="col-12 col-md-10 d-none d-xl-block">
+        <nav class="site-navigation position-relative text-right" role="navigation">
+
+          <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li class="has-children">
+              <a href="services.php">Services</a>
+              <ul class="dropdown">
+                <li><a href="#">Air Freight</a></li>
+                <li><a href="#">Ocean Freight</a></li>
+                <li><a href="#">Ground Shipping</a></li>
+                <li><a href="#">Warehousing</a></li>
+                <li><a href="#">Storage</a></li>
+              </ul>
+            </li>
+            <li><a href="industries.php">Industries</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+
+      <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+      </div>
+
+    </div>
+  </div>
+</header>
